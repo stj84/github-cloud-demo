57deb4423a2fdaf7638c8b871b622b26c0b52ff6
stj <252742164@qq.com>
2023-02-13 20:05:04 +0800

    first init commet

---
+++ test2-service/src/main/java/cn/itcast/test2/Test2ServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.itcast.test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2ServiceApplication.class, args);
+    }
+
+}
